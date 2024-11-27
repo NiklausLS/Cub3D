c7fa7ce84daa6590f7fcd74a7897eaf92b9dcfc0
NiklausLS <nicolas.leempoels@gmail.com>
2024-11-27 21:01:36 +0100

    FIX parsing ceiling/ground rbg

--- parsing/check_rbg.c
+++ parsing/check_rbg.c
@@ -6,7 +6,7 @@
 /*   By: nileempo <nileempo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 23:18:33 by nileempo          #+#    #+#             */
-/*   Updated: 2024/11/25 13:28:04 by nileempo         ###   ########.fr       */
+/*   Updated: 2024/11/27 21:00:49 by nileempo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,12 @@ void	get_rgb_path(t_data *data, char *file)
 	{
 		if (ft_strncmp(line, "F ", 2) == 0)
 		{
-			data->floor = ft_strdup(line);
+			data->floor = ft_strdup(line + 2);
 			printf("F = %s\n", data->floor);
 		}
 		else if (ft_strncmp(line, "C ", 2) == 0)
 		{
-			data->ceiling = ft_strdup(line);
+			data->ceiling = ft_strdup(line + 2);
 			printf("C = %s\n", data->ceiling);
 		}
 		free(line);
@@ -47,23 +47,36 @@ void	get_rgb_path(t_data *data, char *file)
 /*
  * check if the number is between 0 and 255
  */
-/*static int	check_nbr(char *nbr)
+static int	check_nbr(char *nbr)
 {
-	int	res;
-	int i;
+	int		res;
+	int		i;
+	char	*trim;
 
 	i = 0;
-	while (nbr[i])
+	trim = ft_strtrim(nbr, " \t\n\r");
+	if (!trim)
+		return (1);
+	//printf("---check_nbr : nbr = %s\n", nbr);
+	while (trim[i])
 	{
-		if (!ft_isdigit(nbr[i]))
+		if (!ft_isdigit(trim[i]))
+		{
+			//printf("--ERROR: %c is not a nbr in %s\n", trim[i], trim);
+			free(trim);
 			return (1);
+		}
 		i++;
 	}
-	res = ft_atoi(nbr);
+	res = ft_atoi(trim);
+	//printf("res = %d\n", res);
+	free(trim);
+	if (res == 0)
+		return (0);
 	if (res < 0 || res > 255)
 		return (1);
 	return (0);
-}*/
+}
 
 static int	check_split_rbg(char *nbr)
 {
@@ -74,16 +87,26 @@ static int	check_split_rbg(char *nbr)
 	rbg_array = ft_split(nbr, ',');
 	if (!rbg_array)
 		return (1);
+	printf("check_split p1\n");
 	while (rbg_array[i])
 	{
 		printf("rbg_array[%d] = %s\n", i, rbg_array[i]);
+		if (check_nbr(rbg_array[i]) == 1)
+		{
+			printf("ERROR rbg_array[%d] = %s\n", i, rbg_array[i]);
+			free_array(rbg_array);
+			return (1);
+		}
 		i++;
 	}
+	//printf("check_split p2\n");
 	if (i != 3)
 	{
 		free_array(rbg_array);
 		return (1);
 	}
+	//printf("check_split end\n");
+	free_array(rbg_array);
 	return (0);
 }
 
